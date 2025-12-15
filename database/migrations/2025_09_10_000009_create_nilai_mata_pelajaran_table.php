6cca7ee8e830f1e38d70b593331a999d8393594b
EgarDharmawangsa <139887876+EgarDharmawangsa@users.noreply.github.com>
2025-12-15 11:27:30 +0800

    update migrasi nilai mapel

--- database/migrations/2025_09_10_000009_create_nilai_mata_pelajaran_table.php
+++ database/migrations/2025_09_10_000009_create_nilai_mata_pelajaran_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('id_mata_pelajaran')->constrained('mata_pelajaran', 'id_mata_pelajaran')->onDelete('cascade');
             $table->foreignId('id_siswa')->constrained('siswa', 'id_siswa')->onDelete('cascade');
             $table->unique(['id_semester', 'id_mata_pelajaran', 'id_siswa'], 'unique_nilai_mata_pelajaran');
+            $table->integer('jumlah_portofolio')->default(0);
             $table->json('nilai_portofolio');
             $table->integer('nilai_ub')->default(0);
             $table->integer('nilai_uts')->default(0);
