be459145552738049e86bedea5cdbc278bcc8286
Ngurahh <wibawa547294@gmail.com>
2026-01-17 10:10:51 +0800

    Changes to be committed: ubah label "Kata Sandi" menjadi "password"

--- lang/id/passwords.php
+++ lang/id/passwords.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'reset'     => 'Kata sandi Anda sudah direset!',
-    'sent'      => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda!',
+    'reset'     => 'Password Anda sudah direset!',
+    'sent'      => 'Kami sudah mengirim surel yang berisi tautan untuk mereset password Anda!',
     'throttled' => 'Harap tunggu sebelum mencoba lagi.',
-    'token'     => 'Token pengaturan ulang kata sandi tidak sah.',
+    'token'     => 'Token pengaturan ulang password tidak sah.',
     'user'      => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
 ];
